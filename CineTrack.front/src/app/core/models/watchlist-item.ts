f2a4e99c497b4f4693977fce19e9657634ce4c7d
Mahmood Elbadri <mahmoud.elbadry357@gmail.com>
2026-04-09 13:02:54 +0200

    feat: initialize Angular frontend project with core models, navigation, and API configuration

---
+++ CineTrack.front/src/app/core/models/watchlist-item.ts
@@ -0,0 +1,23 @@
+export interface WatchlistItem {
+    id: number;
+    userId: string;
+    tmdbMovieId: number;
+    movieTitle: string;
+    addedAt: Date;
+    isWatched: boolean;
+}
+
+/*
+namespace CineTrack.api.Models;
+
+public class WatchListItem
+{
+    public int Id{ get; set; }
+    public AppUser? User { get; set; }
+    public string UserId { get; set; }
+    public int  TmdbMovieId { get; set; }
+    public string MovieTitle { get; set; }
+    public DateTime AddedAt { get; set; } = DateTime.Now;
+    public bool IsWatched { get; set; }
+}
+*/
