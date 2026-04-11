27ba316a4eb9bde5729de39e9bd3646ba1589989
Mahmood Elbadri <mahmoud.elbadry357@gmail.com>
2026-04-11 13:31:50 +0200

    feat: implement authentication module with login and registration components and routing

--- CineTrack.front/src/app/core/auth/login/login.component.ts
+++ CineTrack.front/src/app/core/auth/login/login.component.ts
@@ -1,14 +1,14 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../services/AuthService';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
--- CineTrack.front/src/app/core/auth/register/register.component.ts
+++ CineTrack.front/src/app/core/auth/register/register.component.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from '../services/AuthService';
+import { Router, RouterLink } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
-export class RegisterComponent {
-
+export class RegisterComponent implements OnInit {
+  
+  //variables
+  protected registerForm!: FormGroup;
+  
+  //injections
+  private authService = inject(AuthService);
+  private router = inject(Router);
+  private fb = inject(FormBuilder);
+  private toastr = inject(ToastrService);
+  
+  //methods
+  ngOnInit(): void {
+    if(this.authService.isAuthenticated()){
+      this.router.navigate(['/movies']);
+    }
+    this.initializeRegisterForm();
+  }
+  
+  initializeRegisterForm(){
+    this.registerForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+  
+  onSubmit() {
+    const model = this.registerForm.value;
+    this.authService.register(model).subscribe({
+      next: () => {
+        this.router.navigate(['/movies']);
+        this.toastr.success('Registration successful');
+      },
+      error: (error) => {
+        console.log(error);
+        this.toastr.error('Registration failed');
+      }
+    });
+  }
 }
